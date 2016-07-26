421148ee3ac86dcf275b99a56ec08c09022a6b6a
ningchanglong <ioi@ioideMac-mini-2.local>
2016-07-26 15:10:37 +0800

    NCL
    测试

---
+++ NCLDemo/UserModel.h
@@ -0,0 +1,13 @@
+//
+//  UserModel.h
+//  NCLDemo
+//
+//  Created by ioi on 16/7/26.
+//  Copyright © 2016年 ncl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserModel : NSObject
+
+@end
---
+++ NCLDemo/UserModel.m
@@ -0,0 +1,13 @@
+//
+//  UserModel.m
+//  NCLDemo
+//
+//  Created by ioi on 16/7/26.
+//  Copyright © 2016年 ncl. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
